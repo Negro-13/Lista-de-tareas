0b3c522e3d09b9600ede63f3829562c6c956807e
Negro_13 <141974651+Negro-13@users.noreply.github.com>
2024-07-23 22:04:34 -0300

    1

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const handleInputChange = (event) => {
+  setInputText(event.target.value);
+  };
+  return (
+    <div className="App">
+    <main className="App-header">
+    <label>Ingresa un texto:</label>
+    <input
+    type="text"
+    value={inputText}
+    onChange={handleInputChange}
+    />
+    <p>Texto ingresado: {inputText}</p>
+    </main>
+    </div>
+    );
+    }
+
+export default App;
